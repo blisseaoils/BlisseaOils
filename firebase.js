6b75e1f26ed37da4b98a7ad06eefbfc0f68a4190
blisseaoils <blisseablend@gmail.com>
2026-04-01 20:45:07 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// YOUR CONFIG (paste from Firebase)
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+
+// Export
+export const auth = getAuth(app);
+export const db = getFirestore(app);
